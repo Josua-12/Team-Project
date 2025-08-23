5fee4d2f033f9077a06dc21c9bd2631ceee94387
Josua-12 <jsua86268@gmail.com>
2025-08-23 17:56:24 +0900

    [JSA] update UserController, User

--- JAVA-ShoppingMall-team2-develop/21. MVCLayerDemo/src/com/shopping/controller/UserController.java
+++ JAVA-ShoppingMall-team2-develop/21. MVCLayerDemo/src/com/shopping/controller/UserController.java
@@ -13,9 +13,7 @@ import com.shopping.service.AuthService;
 import com.shopping.service.OrderService;
 import com.shopping.repository.DefaultFileOrderRepository;
 import com.shopping.repository.FileAdminRepository;
-import com.shopping.repository.FileOrderRepository;
 import com.shopping.repository.FileUserRepository;
-
 
 /**
  * 사용자 관련 UI를 담당하는 컨트롤러 
@@ -36,7 +34,6 @@ public class UserController {
 	    DefaultFileOrderRepository orderRepo = new DefaultFileOrderRepository("data/orders.dat");
 	    this.userService = new UserService(userRepo);
 	    this.authService = new AuthService(userRepo, adminRepo);
-	    this.orderService = new OrderService(orderRepo, null, orderRepo);
 	    this.scanner = new Scanner(System.in);
 	    this.session = session;
 	}
